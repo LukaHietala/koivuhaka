01f73a7f235c95a10d1be33c540f119fb5549560
Luka Hietala <luka.hietala08@gmail.com>
2025-11-25 14:28:13 +0200

    listaa asunnot index sivulla

--- app.lua
+++ app.lua
@@ -1,8 +1,14 @@
 local lapis = require("lapis")
+local db = require("lapis.db")
 local app = lapis.Application()
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+app:enable("etlua")
+app.layout = require "views.layout"
+
+app:get("/", function(self)
+    local res = db.query("SELECT * FROM asunnot")
+    self.asunnot = res
+    return { render = "index" }
 end)
 
 return app
--- config.lua
+++ config.lua
@@ -3,5 +3,8 @@ local config = require("lapis.config")
 config("development", {
   server = "nginx",
   code_cache = "off",
-  num_workers = "1"
+  num_workers = "1",
+  sqlite = {
+    database = "koivuhaka.sqlite",
+  }
 })
